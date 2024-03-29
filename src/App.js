4040648242bfe4d9c2fb81e6cc22dabbfe2ef3e0
Imran <mohdimranshountoo@gmail.com>
2024-03-29 12:48:40 +0530

    code was updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import BasicForm from "./components/BasicForm";
 import AdvancedForm from "./components/AdvancedForm";
-import Checkbox from "./components/checkBoxForm"
+import Checkbox from "./components/checkBoxForm";
 
-import TagSelector from "./components/searchFilter"
+import TagSelector from "./components/searchFilter";
+import TagSelector2 from  "./components/searchFilter2"
 import "./App.css";
 
 function App() {
@@ -34,12 +35,23 @@ function App() {
           onClick={() => setView("searchfilter")}
           style={{ color: view === "searchfilter" ? "#fff" : "" }}
         >
- Search Filter
+          
+          Search Filter
         </h3>
+        <h3
+            onClick={() => setView("searchfilter2")}
+            style={{ color: view === "searchfilter2" ? "#fff" : "" }}
+          >Search filter 2</h3>
       </nav>
-      {view === "basic" ? <BasicForm /> : view === "advanced" ? <AdvancedForm /> : view === "checkbox" ? <Checkbox /> : <TagSelector/>}
-
-
+      {view === "basic" ? (
+        <BasicForm />
+      ) : view === "advanced" ? (
+        <AdvancedForm />
+      ) : view === "checkbox" ? (
+        <Checkbox />
+      ) : view === "searchfilter" ? (
+        <TagSelector />
+      ): <TagSelector2/>}
     </div>
   );
 }
